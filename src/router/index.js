a509a8ce554ff4c107583585d1274e3301eca70b
李杨 <1711467488@qq.com>
2018-03-05 20:05:02 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/dashboard',
+      name: 'dashboard',
+      component: resolve => { require(['@/page/dashboard/index.vue'], resolve) }
     }
   ]
 })
