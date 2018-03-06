5b117be5bce87ad237eaed293b0054c91877dde8
李杨 <1711467488@qq.com>
2018-03-06 11:06:51 +0800

    no message

--- src/components/cdds/maker/index.vue
+++ src/components/cdds/maker/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
+  <div class="maker" :style="style">
     maker
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    height: {
+      type: String,
+      required: false,
+      default: '720px'
+    }
+  },
+  computed: {
+    style () {
+      return {
+        height: this.height
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.maker {
+  background-color: #CCC;
+}
+</style>
--- src/page/dashboard/index.vue
+++ src/page/dashboard/index.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    dashboard
+    <!-- 编辑器 -->
+    <cdds-maker></cdds-maker>
   </div>
 </template>
+
+<script>
+export default {
+  components: {
+    CddsMaker: () => import('@/components/cdds/maker/index.vue')
+  }
+}
+</script>
